ab995c86785cc576a30659692b420077f59704dd
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-03-14 18:22:23 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+var express = require('express');
+var app = express();
+var http = require('http');
+var path = require('path');
+var async = require('async');
+var fs = require('fs');
+var socketio = require('socket.io');
+var port = process.env.PORT || 443;
+var passport = require('passport');
+var server = http.createServer(app).listen(port);
+var socket = socketio.listen(server);
