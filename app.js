e0cf68fbfbb0e65c20a6fcebe8e576a48e9d0a14
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-03-17 20:07:55 -0400

    Add mongoose variables and create mongoose connections

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const http = require('http');
 const path = require('path');
 const async = require('async');
 const fs = require('fs');
+const mongoose = require('mongoose');
 const io = require('socket.io');
 const port = 443;
 const passport = require('passport');
 const server = http.createServer(app).listen(port);
 const socket = io.listen(server);
+
+mongoose.connect(MONGODB_URL_ChatApp);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
 
 app.use(express.static('public'));
 
