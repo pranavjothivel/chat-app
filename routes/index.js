abaf1be25ca530d47d0dde42f532eba12bbe74aa
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-03-27 18:50:09 -0400

    Include auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-
+const auth = require('./auth')
 
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views', 'index.ejs'));
