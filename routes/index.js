0aa980e94e0b354770e76ec01cac2a776aa28b51
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-03-30 19:11:10 -0400

    Changes

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('./auth');
 
+router.use('/', auth);
+
 router.get('/', function (req, res) {
-    res.render('index.ejs', {
-        title: "Timetalk",
+    res.render('index.ejs',{
+        title: 'Timetalk',
         metadata: null,
     });
 });
 
 module.exports = router;
-module.exports = auth;
