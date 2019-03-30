bc8b0444b0a061c7a4a96e2a59eb8d8082663491
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-03-30 17:46:56 -0400

    Pass data to page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('./auth')
+const auth = require('./auth');
 
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'index.ejs'));
-})
+    res.render('index.ejs', {
+        title: "Talktime",
+        metadata: null,
+    });
+});
 
 module.exports = router;
