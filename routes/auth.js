8153753ca1ac2bb040a9be2564b8bbcb0fb61a6c
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-04-03 16:22:53 -0400

    Add routes for post

--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,19 @@ router.get('/login', function (req, res) {
         metadata: null,
     });
 });
+router.post('/login', function (req, res, next) {
+
+})
 
 router.get('/signup', function (req, res) {
     res.render('signup.ejs',{
         title: "Signup - Timetalk",
         metadata: null,
+        TOKEN: "sample",
     });
 });
+router.post('/signup', function (req, res, next) {
+    
+})
 
 module.exports = router;
