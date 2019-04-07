c401f93f60ac6965f2247bc7b64e6d621225dfdf
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-04-07 19:49:34 -0400

    Add support for localhost ssl

--- app.js
+++ app.js
@@ -3,38 +3,34 @@ const dotenv = require('dotenv').config();
 const ENV = process.env.NODE_ENV;
 
 const express = require('express');
-const app = express();
+const app = module.exports = express();
 
 const path = require('path');
 const async = require('async');
 const fs = require('fs');
 
-const SSL_CERT = fs.readFileSync(process.env.CERTIFICATE);
-const SSL_KEY = fs.readFileSync(process.env.CERTIFICATE_KEY);
-
-const SSL_CONFIG = {
-    key: SSL_KEY,
-    cert: SSL_CERT
-};
-
-
-const https = require('https');
 const http = require('http');
+const https = require('https');
 
 const HTTP_PORT = process.env.HTTP_PORT;
-const HTTPS_PORT = process.env.HTTPS_PORT;
-const SOCKET_PORT = process.env.SOCKET_PORT;
-
 const httpServer = http.createServer(app).listen(HTTP_PORT);
-// const httpsServer = https.createServer(SSL_CONFIG, app).listen(HTTPS_PORT);
+
+Enable_SSL_Local();
 
 const mongoose = require('mongoose');
 
+const SOCKET_PORT = process.env.SOCKET_PORT;
 const io = require('socket.io');
 const passport = require('passport');
 const sendgrid = require('@sendgrid/mail');
 const socket = io.listen(SOCKET_PORT);
 const routes = require('./routes');
+
+function Enable_SSL_Local() {
+    if(ENV == 'dev' || 'development' && process.env.PRODUCTION_ENV == 'localhost'){
+        require('./scripts/ENABLE_SSL_LOCAL');
+    }
+}
 
 app.use('*', function (req, res, next) {
     if(req.secure){
