1bda709c9ab31e359d69817dc153979178b95eba
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-04-08 19:08:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,18 +16,6 @@ const sendgrid = require('@sendgrid/mail');
 const socket = io.listen(SOCKET_PORT);
 const routes = require('./routes');
 
-app.use('*', function (req, res, next) {
-    if(req.secure){
-        next();
-    }
-    else if(ENV == 'production' && process.env.PRODUCTION_ENV == 'nginx') {
-        res.redirect('http://' + req.headers.host + req.url);
-    }
-    else {
-        res.redirect('https://' + req.headers.host + req.url);
-    }
-});
-
 app.set('view engine', 'hbs')
 app.use(express.static('public'));
 app.use('/', routes);
