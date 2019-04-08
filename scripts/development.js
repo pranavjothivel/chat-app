80c47fdbdeec6883253be0cac8e2f1fdad90bdab
Pranav Jothivel <pranav.jothivel@outlook.com>
2019-04-08 19:08:39 -0400

    Remove scripts

--- scripts/development.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-require('dotenv').config();
-
-const app = require('../app');
-
-const http = require('http');
-
-const HTTP_PORT = process.env.HTTP_PORT;
-const httpServer = http.createServer(app).listen(HTTP_PORT);
--- scripts/production.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const app = require('../app');
-
-const http = require('http');
-
-const HTTP_PORT = process.env.HTTP_PORT;
-const httpServer = http.createServer(app).listen(HTTP_PORT);
